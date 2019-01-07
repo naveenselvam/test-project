8d68a383d0febab4aa875f2680733f60d7107e3f
Navi <selvam.naveen@yahoo.com>
2019-01-06 22:05:17 -0800

    Date changes

--- Samples/src/com/navi/DateUtils/DateUtils.java
+++ Samples/src/com/navi/DateUtils/DateUtils.java
@@ -7,6 +7,7 @@ public class DateUtils {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// dev comments
+		// main comments added
 			System.out.println("date"+ new Date());
 	}
 
